099935a037c61ce54d74f94fda817fcaf44ff454
amshel hack3r <amshelhack3r@gmail.com>
2020-11-20 09:04:54 +0300

    Add app constants

---
+++ app/src/main/java/com/example/nytimesreader/util/constants.kt
@@ -0,0 +1,7 @@
+package com.example.nytimesreader.util
+
+//get our base url for the nytimes
+const val BASE_URL = "https://api.nytimes.com/svc/topstories/v2/"
+
+//get our api key
+const val API_KEY = "e1vpJ8bfcUbyEr1fJsmlPG1XUi2A4VHC"
